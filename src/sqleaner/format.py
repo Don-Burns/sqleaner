e402c48d39dd35a875edff5693997952e8ab4798
Donal Burns <donal.w.burns@hotmail.com>
2023-10-18 12:44:17 +0100

    Shift towards visitor stype walk

--- src/sqleaner/format.py
+++ src/sqleaner/format.py
@@ -31,6 +31,95 @@ class ColumnBlock:
     alias: Optional[str] = None
 
 
+class ExpressionKey(Enum):
+    """
+    Keys returned when walking the AST.
+
+    Attributes:
+        EXPRESSIONS (str): Key for expressions, e.g. at the top level of a select statement or a column, means there is more data nested inside
+        EXPRESSION (str): key for expression
+        NONE (None): key for None
+        TABLE (str): key for table, specialisation of `this` for table alias tokens
+        THIS (str): key for this, means the current path has reached an end or is an identifier
+        FROM (str): key for from, specialisation of `this` for from token
+        ALIAS (str): key for alias, specialisation of `this` for alias token, e.g `AS t`
+        JOINS (str): key for joins, like expressions but for joins specifically
+            i.e. its nested
+        WITH (str): key for with
+        ON (str): key for on
+    """
+
+    EXPRESSIONS = "expressions"
+    EXPRESSION = "expression"
+    NONE = None
+    TABLE = "table"
+    THIS = "this"
+    FROM = "from"
+    ALIAS = "alias"
+    JOINS = "joins"
+    WITH = "with"
+    ON = "on"
+
+
+class ExpressionVisitor:
+    def __init__(self, col_sep: str, indent_chars: str) -> None:
+        self.col_sep = col_sep
+        self.indent_chars = indent_chars
+        self.visited_nodes: list[expressions.Expression] = []
+
+    def visit_expression(self, exp: expressions.Expression) -> str:
+        """
+        Handle a node in the AST.
+        If formatting the node, the formatted string is returned.
+        Else an empty string is returned.
+        Means that the caller can just append the return value to the output string
+        while walking the ast.
+
+        Args:
+            exp (expressions.Expression):
+
+        Returns:
+            str: empty string if the node is not formatted/skipped, else the formatted sql string
+        """
+        sql = ""
+        # if the node is too below a level we have already handled then skip it
+        if self._skip_node(exp) is True:
+            return ""
+
+        # if the node gets formatted all its children will be too, so add them to the visited nodes
+        sql = self.route_expression(exp)
+        # only want the nodes added to the visited nodes if they are formatted
+        # don't care about key or parent node (2nd and 3rd items in tuple)
+        self.visited_nodes.extend([node for node, *_ in exp.dfs()])  # type: ignore [no-untyped-call]
+        return sql
+
+    def route_expression(self, exp: expressions.Expression) -> str:
+        match exp:
+            case _ if isinstance(exp, expressions.Select):
+                formatted_exp = _format_select(exp, self.col_sep, self.indent_chars)
+            case _:
+                logger.warning(
+                    "Expression handling not implemented: %s. Falling back to default formatting",
+                    exp,
+                )
+                formatted_exp = exp.sql()
+        return formatted_exp
+
+    def _skip_node(self, node: expressions.Expression) -> bool:
+        """
+        Filter for the types of expressions to skip.
+
+        Args:
+            node (expressions.Expression):
+
+        Returns:
+            bool:
+        """
+        if node in self.visited_nodes:
+            return True
+        return False
+
+
 def format_sql(sql_str: str) -> str:
     parsed = _parse_statement(sql_str)
     output: list[str] = []
@@ -43,13 +132,25 @@ def format_sql(sql_str: str) -> str:
         # set indentation
         indent_chars = " " * 4 * (indent_level + 1)
         col_sep = f"\n{indent_chars}{base_col_sep} "
-        logger.debug("current expression: %s", exp)
+        formatted_exp = ""
+        logger.debug("current expression: (type: %s) %s", type(exp), exp)
         # TODO start using the walk/dfs methods to iterate over the AST instead of manually handling each expression type
-        match exp:
-            case _ if isinstance(exp, expressions.Select):
-                formatted_exp = __format_select(exp, col_sep, indent_chars)
-            case _:
-                raise NotImplementedError(f"Expression handling not implemented: {exp}")
+        visitor = ExpressionVisitor(col_sep=col_sep, indent_chars=indent_chars)
+        for node, parent_node, raw_key in exp.dfs():  # type: ignore [no-untyped-call]
+            # narrow types from the walk
+            if not isinstance(node, expressions.Expression):
+                raise RuntimeError("Node is not an expression")
+            if (
+                not isinstance(parent_node, expressions.Expression)
+                and parent_node is not None
+            ):
+                raise RuntimeError("Parent node is not an expression")
+            try:
+                key = ExpressionKey(raw_key)
+            except ValueError:
+                raise RuntimeError(f"{raw_key} is not a valid ExpressionKey")
+
+            formatted_exp += visitor.visit_expression(node)
 
         # add ending e.g. semi-colon
         formatted_exp += expression_ending
@@ -93,7 +194,7 @@ def _parse_statement(sql_str: str) -> list[expressions.Expression | None]:
     return parsed
 
 
-def __format_select(
+def _format_select(
     exp: expressions.Select, col_sep: str, indentation_chars: str
 ) -> str:
     """
